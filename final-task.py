c171110edf459b71c1058da000bf545535b29f52
chris-reeves1 <dr_gonzo19@hotmail.com>
2024-03-19 10:34:36 +0000

    2

---
+++ final-task.py
@@ -0,0 +1,15 @@
+# write an application that checks the strength of a password.
+# Requirements:
+# 
+# Write a class that has a method that checks the password strength.
+# Use factors like length, upper/lower case and if it has a number and special character.
+# ratings should be very weak - weak - moderate - strong - very strong 
+# Check against a list of common passwords 10-20 common password = very weak
+# User input that loops until the user quits
+# A dictionary that returns a history of passwords/strengths whilst in the loop.
+
+# Code must be pushed to a repo and 1 person per group must share with me. 
+
+# write tests and achieve a high score in pylint. 
+
+# 12.30 deadline
